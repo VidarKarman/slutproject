7cad12f9ddbc683e0e41718dbc9d1203f98ba288
VidarKarman <vidar.karman@elev.ga.ntig.se>
2025-03-14 14:24:02 +0100

    efter lektionen v 11

---
+++ js/mainjs.js
@@ -0,0 +1,7 @@
+function showMenu(){
+    let menu = document.querySelector("nav.menu")
+    menu.classList.toggle("show")
+
+    let layer = document.querySelector(".fade-layer")
+    layer.classList.toggle("visible")
+}
