fc5db7325dca876b1685fb9b8b387eea64180474
VidarKarman <vidar.karman@elev.ga.ntig.se>
2025-05-07 18:59:57 +0200

    Update mainjs.js

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,3 +1,8 @@
+//let minRubrik = document.querySelector(”h1”)
+
+// Ändra h1’s css
+//minRubrik.classList.toggle(”show”) 
+
 function showMenu(){
     let menu = document.querySelector("nav.menu")
     menu.classList.toggle("show")
